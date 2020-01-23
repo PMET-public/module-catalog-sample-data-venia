ffc0a41b04afe5c4a910fb3aca6a53783be2c61b
Jeff Britts <jbritts@adobe.com>
2020-01-23 16:59:37 -0500

    added downloadables with test data

---
+++ Setup/Patch/Data/InstallDownloadableProducts.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\CatalogSampleDataVenia\Setup\Patch\Data;
+
+
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup;
+use Magento\CatalogSampleDataVenia\Setup\Category;
+use Magento\CatalogSampleDataVenia\Setup\Attribute;
+use Magento\CatalogSampleDataVenia\Model\Downloadable\Product;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Store\Model\Store;
+
+class InstallDownloadableProducts implements DataPatchInterface
+{
+
+    /**
+     * @var \Magento\CatalogSampleData\Model\Category
+     */
+    protected $category;
+
+    /**
+     * @var \Magento\CatalogSampleData\Model\Attribute
+     */
+    private $attribute;
+
+    /**
+     * @var \Magento\DownloadableSampleData\Model\Product
+     */
+    private $downloadableProduct;
+
+    /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
+
+    /**
+     * InstallDownloadableProducts constructor.
+     * @param Category $category
+     * @param Attribute $attribute
+     * @param Product $product
+     * @param StoreManagerInterface|null $storeManager
+     */
+    public function __construct(
+        Category $category,
+        Attribute $attribute,
+        Product $product,
+        StoreManagerInterface $storeManager = null
+    ) {
+        $this->category = $category;
+        $this->attribute = $attribute;
+        $this->downloadableProduct = $product;
+        $this->storeManager = $storeManager ?: \Magento\Framework\App\ObjectManager::getInstance()
+            ->get(StoreManagerInterface::class);
+    }
+
+    public function apply()
+    {
+        $this->storeManager->setCurrentStore(Store::DISTRO_STORE_ID);
+
+        $this->attribute->install(['Magento_CatalogSampleDataVenia::fixtures/Downloadable/attributes.csv']);
+        $this->category->install(['Magento_CatalogSampleDataVenia::fixtures/Downloadable/categories.csv']);
+        $this->downloadableProduct->install(
+            ['Magento_CatalogSampleDataVenia::fixtures/Downloadable/products_video_download.csv'],
+            ['Magento_CatalogSampleDataVenia::fixtures/Downloadable/images_products_video.csv'],
+            ['Magento_CatalogSampleDataVenia::fixtures/Downloadable/downloadable_data_video_download.csv']
+        );
+    }
+
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    public function getAliases()
+    {
+        return [];
+    }
+}
